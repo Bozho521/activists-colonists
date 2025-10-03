991c1089fc9b81b8f8afaf00178678a293965b8e
Bozho521 <115819440+Bozho521@users.noreply.github.com>
2025-10-03 23:41:45 +0200

    Player_Raycast

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // new input system
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float rayDistance = 100f;
+    private Camera playerCamera;
+
     void Start()
     {
-        
+        playerCamera = Camera.main;
+        if (playerCamera == null)
+        {
+            Debug.LogError("No Main Camera found in the scene! Please tag your camera as 'MainCamera'.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (playerCamera == null) return;
+
+            Ray ray = playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                Tile tile = hit.collider.GetComponent<Tile>();
+                if (tile != null)
+                {
+                    Destroy(tile.gameObject);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnDestroy()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(gameObject.name + " was destroyed!");
     }
 }
