cbb4fb53d935da68bee5cb4b8acbf70f36f18f57
Christos Krilis <chkri@itu.dk>
2025-10-05 01:56:44 +0200

    add on hover functionality

--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Data;
 using UnityEngine;
 using Enums;
-using Mono.Cecil;
 
 namespace Tiles
 {
@@ -17,8 +16,17 @@ namespace Tiles
 
         [Tooltip("Assign neighbor tiles here (6 max for hex).")]
         [SerializeField] private List<Tile> neighbors = new List<Tile>();
-        
+
+        [Header("Hover Outline")]
+        [SerializeField] private Material outlineMaterial;
+        [SerializeField] private Renderer outlineRenderer;
+        [SerializeField] private float outlineYOffset = 0.004f;
+        [SerializeField] private Color outlineColor = Color.white;
+        [SerializeField] private string outlineColorProperty = "_BaseColor";
+
         private Renderer _renderer;
+        private MaterialPropertyBlock _mpb;
+
         public TileOwner Owner => owner;
         public bool IsBuildable => buildable;
         public TileConfig Config => config;
@@ -26,15 +34,21 @@ namespace Tiles
 
         private void Awake()
         {
-            _renderer = GetComponentInChildren<Renderer>();
+            _mpb = new MaterialPropertyBlock();
+
+            _renderer = currentGraphics
+                ? currentGraphics.GetComponentInChildren<Renderer>()
+                : GetComponentInChildren<Renderer>();
+
             ApplyVisual();
+            EnsureOutline();
+            SetHover(false);
         }
 
         internal void Bind(HexGridManager grid)
         {
-            
         }
-        
+
         public void SetOwner(TileOwner newOwner)
         {
             if (owner == newOwner) return;
@@ -45,29 +59,115 @@ namespace Tiles
         public void SetBuildable(bool value)
         {
             buildable = value;
-            // TODO: dim/outline, etc.
+            if (_renderer != null)
+            {
+                _renderer.GetPropertyBlock(_mpb);
+                float dim = buildable ? 1f : 0.8f;
+                _mpb.SetFloat("_Surface", 0f);
+                _renderer.SetPropertyBlock(_mpb);
+                _renderer.sharedMaterial = _renderer.sharedMaterial;
+                _renderer.transform.localScale = Vector3.one * (buildable ? 1f : 0.995f);
+            }
         }
-        
+
         public void ApplyVisualTile(GameObject tilePrefab)
         {
+            if (currentGraphics == null)
+                currentGraphics = this.gameObject;
+
             if (currentGraphics.transform.childCount > 0)
-            {
                 Destroy(currentGraphics.transform.GetChild(0).gameObject);
-            }
-            
+
             var go = Instantiate(tilePrefab, currentGraphics.transform);
             go.transform.localPosition = Vector3.zero;
             go.transform.localScale = Vector3.one * 1.01f;
             go.transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
-        }
 
+            _renderer = currentGraphics.GetComponentInChildren<Renderer>();
+            ApplyVisual();
+
+            EnsureOutline(forceRecreate: true);
+            SetHover(false);
+        }
 
         private void ApplyVisual()
         {
             if (_renderer == null || config == null) return;
-            var mat = config.neutralMat;
-            if (mat)
+
+            var mat = owner switch
+            {
+                TileOwner.P1 => config.p1OwnedMat ? config.p1OwnedMat : config.neutralMat,
+                TileOwner.P2 => config.p2OwnedMat ? config.p2OwnedMat : config.neutralMat,
+                _            => config.neutralMat
+            };
+
+            if (mat != null)
                 _renderer.sharedMaterial = mat;
+        }
+
+        public void SetHover(bool on)
+        {
+            if (!outlineRenderer) return;
+            outlineRenderer.enabled = on;
+            if (on)
+            {
+                var mpb = new MaterialPropertyBlock();
+                outlineRenderer.GetPropertyBlock(mpb);
+                mpb.SetColor(outlineColorProperty, outlineColor);
+                outlineRenderer.SetPropertyBlock(mpb);
+            }
+        }
+
+        public void SetHoverColor(Color c)
+        {
+            outlineColor = c;
+            if (!outlineRenderer || !outlineRenderer.enabled) return;
+            var mpb = new MaterialPropertyBlock();
+            outlineRenderer.GetPropertyBlock(mpb);
+            mpb.SetColor(outlineColorProperty, outlineColor);
+            outlineRenderer.SetPropertyBlock(mpb);
+        }
+
+        private void EnsureOutline(bool forceRecreate = false)
+        {
+            if (forceRecreate && outlineRenderer)
+            {
+                DestroyImmediate(outlineRenderer.gameObject);
+                outlineRenderer = null;
+            }
+
+            if (outlineRenderer) return;
+            if (!outlineMaterial) return;
+
+            var sourceRenderer = currentGraphics
+                ? currentGraphics.GetComponentInChildren<Renderer>()
+                : GetComponentInChildren<Renderer>();
+            if (!sourceRenderer) return;
+
+            Mesh sourceMesh = null;
+            if (sourceRenderer.TryGetComponent<MeshFilter>(out var srcMf) && srcMf.sharedMesh)
+                sourceMesh = srcMf.sharedMesh;
+            else if (sourceRenderer is SkinnedMeshRenderer skinned && skinned.sharedMesh)
+                sourceMesh = skinned.sharedMesh;
+
+            if (!sourceMesh) return;
+
+            var go = new GameObject("Outline");
+            go.transform.SetParent(transform, false);
+            go.transform.localPosition = Vector3.up * outlineYOffset;
+            go.transform.localRotation = Quaternion.identity;
+            go.transform.localScale = Vector3.one;
+
+            var mf = go.AddComponent<MeshFilter>();
+            mf.sharedMesh = sourceMesh;
+
+            var mr = go.AddComponent<MeshRenderer>();
+            mr.sharedMaterial = outlineMaterial;
+            mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            mr.receiveShadows = false;
+            mr.enabled = false;
+
+            outlineRenderer = mr;
         }
 
 #if UNITY_EDITOR
---
+++ Assets/Scripts/Tiles/TileHoverHighlighter.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Tiles;
+
+[DefaultExecutionOrder(100)]
+public class TileHoverHighlighter : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask tileMask = ~0;
+    [SerializeField] private float rayDistance = 1000f;
+
+    Tile _current;
+
+    void Awake()
+    {
+        if (!cam) cam = Camera.main;
+    }
+
+    void Update()
+    {
+        var mouse = Mouse.current;
+        if (mouse == null || cam == null) return;
+
+        var ray = cam.ScreenPointToRay(mouse.position.ReadValue());
+        if (Physics.Raycast(ray, out var hit, rayDistance, tileMask, QueryTriggerInteraction.Ignore))
+        {
+            var tile = hit.collider.GetComponentInParent<Tile>();
+            if (tile != _current)
+            {
+                if (_current) _current.SetHover(false);
+                _current = tile;
+                if (_current) _current.SetHover(true);
+            }
+        }
+        else
+        {
+            if (_current) _current.SetHover(false);
+            _current = null;
+        }
+    }
+
+    public void ForceClear()
+    {
+        if (_current) _current.SetHover(false);
+        _current = null;
+    }
+}
